d08a0b3d4156d27ab0af9ad978738a957292653c
Ryan_r <r27santos.com@gmail.com>
2026-05-15 15:58:18 -0300

    substituição da lifeBar pelo HeartController
    Modificações no sistema de vida do player, sistema de corações no lugar da barra de hp

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,39 +1,74 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
-public class PlayerHealth : MonoBehaviour
+public class HealthSystem : MonoBehaviour
 {
-    public Image healthFill;
+    public int currentHealth = 10;
+    public int maxHealth = 10;
 
-    public float maxHealth = 100;
-    private float currentHealth;
+    public Image[] HeartImage;
+
+    public Sprite FullHeart;
+    public Sprite HalfHeart;
+    public Sprite EmptyHeart;
 
     void Start()
     {
         currentHealth = maxHealth;
-        UpdateHealthBar();
+        healthLogic();
     }
 
     void Update()
     {
-        // TESTE
-        if(Input.GetKeyDown(KeyCode.H))
+        // J = meio coração
+        if (Keyboard.current.jKey.wasPressedThisFrame)
         {
-            TakeDamage(10);
+            ChangeHealth(-1);
         }
+
+        // K = um coração
+        if (Keyboard.current.kKey.wasPressedThisFrame)
+        {
+            ChangeHealth(-2);
+        }
+
+        // R = restaura tudo
+        if (Keyboard.current.rKey.wasPressedThisFrame)
+        {
+            currentHealth = maxHealth;
+            healthLogic();
+        }
+
+            if (Keyboard.current.jKey.wasPressedThisFrame) {
+            Debug.Log("J apertado");
+        }
+
     }
 
-    public void TakeDamage(float damage)
-    {
-        currentHealth -= damage;
+    public void ChangeHealth(int amount) {
+        currentHealth += amount;
 
         currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
 
-        UpdateHealthBar();
+        Debug.Log("Vida atual: " + currentHealth);
+
+        healthLogic();
     }
 
-    void UpdateHealthBar()
+    void healthLogic()
     {
-        healthFill.fillAmount = currentHealth / maxHealth;
+        for (int i = 0; i < HeartImage.Length; i++)
+        {
+            if (currentHealth >= (i + 1) * 2)
+                HeartImage[i].sprite = FullHeart;
+
+            else if (currentHealth == (i * 2) + 1)
+                HeartImage[i].sprite = HalfHeart;
+
+            else
+                HeartImage[i].sprite = EmptyHeart;
+        }
     }
+
 }
