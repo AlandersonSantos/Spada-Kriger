860e9add0de364fe66a5791bb4b0db3cbbdea9d2
daniel2311x <daniel.manoel2311@hotmail.com>
2026-06-11 00:33:30 -0300

    Começando script
    Scripit de tela inicial.

---
+++ Assets/Scripts/NewEmptyCSharpScript.cs
@@ -0,0 +1,16 @@
+using System.Collecions;
+using System.Collecions.Generic;
+using UnityEngine;
+
+public class NewEmptyCSharpScript
+{
+    public void Jogar()
+    {
+        
+    }
+
+    public void Sair()
+    {
+        
+    }
+}
